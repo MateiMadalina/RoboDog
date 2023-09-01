ae942055a67308670e568aa60ae710ed30920560
MateiMadalina <116349352+MateiMadalina@users.noreply.github.com>
2023-09-01 15:16:48 +0300

    DogController and http request added

---
+++ src/main/java/com/codecool/robodog/controller/DogController.java
@@ -0,0 +1,45 @@
+package com.codecool.robodog.controller;
+
+import com.codecool.robodog.model.Dog;
+import com.codecool.robodog.storage.DogStorage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/")
+public class DogController {
+ private final DogStorage dogStorage;
+
+    @Autowired
+    public DogController(DogStorage dogStorage) {
+        this.dogStorage = dogStorage;
+    }
+
+    @GetMapping(value = "dogs")
+    public List<Dog> displayAllDog(){
+        return dogStorage.getDogs();
+    }
+
+    @GetMapping(value = "randomDog")
+    public Dog displayRandomDog(){
+        return dogStorage.createAndAddARandomDog();
+    }
+
+    @PostMapping(value = "/newDog")
+    public void addDog(@RequestBody Dog dog){
+        dogStorage.addADog(dog);
+        System.out.println("The dog was added!");
+    }
+
+    @PutMapping(value = "/updateDog/{name}")
+    public void updateDog(@PathVariable String name, @RequestBody Dog updatedDog) {
+        dogStorage.updateDogByName(name, updatedDog);
+        System.out.println("The dog was updated!");
+    }
+
+
+
+
+}
--- src/main/java/com/codecool/robodog/storage/DogStorage.java
+++ src/main/java/com/codecool/robodog/storage/DogStorage.java
@@ -3,36 +3,38 @@ package com.codecool.robodog.storage;
 import com.codecool.robodog.model.Breed;
 import com.codecool.robodog.model.Dog;
 import com.codecool.robodog.service.DogCreator;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.stream.Collectors;
-
+@Component
 public class DogStorage {
    private List<Dog> dogs;
    private DogCreator dogCreator;
 
-    public DogStorage(List<Dog> dogs, DogCreator dogCreator) {
+    public DogStorage(List<Dog> dogs) {
         this.dogs = dogs;
-        this.dogCreator = dogCreator;
+        this.dogCreator = new DogCreator();
     }
 
     public void addADog(Dog dog){
         dogs.add(dog);
     }
 
-    public void createAndAddARandomDog(){
+    public Dog createAndAddARandomDog(){
         Dog randomDog = dogCreator.createRandomDog();
         dogs.add(randomDog);
+        return randomDog;
     }
 
     public List<Dog> getDogs() {
         return dogs;
     }
 
-    public void updateAgeAndBreedUsingByTheName(String name, int age, Breed breed){
+    public void updateDogByName(String name, Dog updatedDog){
         List<Dog> dogsFound = dogs.stream().filter(dog -> dog.getName().equals(name)).collect(Collectors.toList());
         Dog firstDog = dogsFound.get(0);
-        firstDog.setAge(age);
-        firstDog.setBreed(breed);
+        firstDog.setAge(updatedDog.getAge());
+        firstDog.setBreed(updatedDog.getBreed());
     }
 }
